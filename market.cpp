3ff2f5a6d7108782060b25b18558bab3b3b4522d
psrivast <psrivast@andrew.cmu.edu>
2016-06-12 14:14:56 -0400

    Added verification functions and mechanism to check commands

--- market.cpp
+++ market.cpp
@@ -6,6 +6,31 @@
 
 using namespace std;
 
+/* Verification functions */
+bool verify_dealer_id(string dealer_id) {
+    for(int i = 0; i < sizeof(dealers)/sizeof(*dealers); i++) {
+        if(dealers[i] == dealer_id) return true;
+    }
+    return false;
+}
+
+bool verify_side(string side) {
+    for(int i = 0; i < sizeof(sides)/sizeof(*sides); i++) {
+        if(sides[i] == side) return true;
+    }
+    return false;
+
+}
+ 
+bool verify_commodity(string commodity) {
+    for(int i = 0; i < sizeof(commodities)/sizeof(*commodities); i++) {
+        if(commodities[i] == commodity) return true;
+    }
+    return false;
+
+}
+
+
 void process_message(string message) {
     vector<string> tokens;
     istringstream iss(message);
@@ -14,10 +39,28 @@ void process_message(string message) {
         istream_iterator<string>(),
         back_inserter(tokens));    
     
-    if(tokens.size() < 2) cout << "Error: Invalid message" << endl;  
-
-    string dealer_id = tokens[0];
+    if(tokens.size() < 2) {
+        cout << "ERROR: Invalid message" << endl;  
+        return;    
+    }
     
+    if(!verify_dealer_id(tokens[0])) {
+        cout << "ERROR: Unkonwn Dealer: " << tokens[0] << endl;
+        return;
+    }   
+   
+    if(tokens[1] == "POST") post_command(tokens);
+    /*
+    else if(tokens[1] == "REVOKE") revoke_command(tokens);
+    else if(tokens[1] == "CHECK") check_command(tokens);
+    else if(tokens[1] == "LIST") list_command(tokens);
+    else if(tokens[1] == "AGGRESS") aggress_command(tokens);
+    */    
+    else {
+        cout << "ERROR: Invalid Message: Unknown command " << tokens[1] << endl;
+        return; 
+    }   
+     
     /*
     for(vector<int>::size_type i = 0; i < tokens.size(); i++) {
         cout << tokens[i] << " | ";    
@@ -25,10 +68,11 @@ void process_message(string message) {
     */
 }
 
-
+void post_command(vector<string> tokens) {
+}
 
 int main() {
-    
+   
     while (true) {
         string message;
         getline(cin, message);
--- market.h
+++ market.h
@@ -1,32 +1,44 @@
 
 using namespace std;
 
-enum Side {
-    BUY, 
-    SELL
+const string sides[] = {
+    "BUY", 
+    "SELL"
 };
 
-enum Commodity {
-    GOLD,
-    SILV,
-    PORK,
-    OIL,
-    RICE
+const string commodities[] = {
+    "GOLD",
+    "SILV",
+    "PORK",
+    "OIL",
+    "RICE"
 };
 
-enum Dealer {
-    DB,
-    JPM,
-    UBS,
-    RBC,
-    BARX
+const string dealers[] = {
+    "DB",
+    "JPM",
+    "UBS",
+    "RBC",
+    "BARX"
 };
 
 class Order {
 public:
     int order_id;
-    Side side;
-    Commodity comm;
+    string side;
+    string comm;
     int quant;
+    string dealer_id;
     double price;
 };
+
+/* Verification function */
+bool verify_dealer_id(string dealer_id);
+bool verify_side(string side);
+bool verify_commodity(string commodity);
+
+/* Message processing */
+void process_message(string message);
+
+/* POST */
+void post_command(vector<string> tokens);
